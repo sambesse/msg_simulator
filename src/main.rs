a6a9c482272d0b5e9881f287c2cb7d780d97473f
Sam Besse <sam.besse@gmail.com>
2025-08-04 22:38:26 -0400

    attempting to start using nix for open, using more rust friendly naming conventions

--- src/main.rs
+++ src/main.rs
@@ -9,40 +9,38 @@
 *   and allow a user to modify the fields via a terminal interface
 *
 * this is currently designed for simulating CAN messages, but would be
-* nice to give the flexibility to allow any type of packet over any 
+* nice to give the flexibility to allow any type of packet over any
 * interface
 */
-
 use nix; //for now just do the whole crate
+use nix::fcntl;
+use nix::fcntl::{OFlag, open};
+use std::os::fd::RawFd;
 
-enum packet_type {
-    PERIODIC, // sent every period
-    ON_CHANGE, // only sent when a data signal changes
+enum PacketType {
+    Periodic, // sent every period
+    OnChange, // only sent when a data signal changes
 }
 
-struct data_signal_desc {
+struct DataSignalDesc {
     start_bit: u16, //soft limits packet length to 8KB
-    length: u16
+    length: u16,
 }
 
-struct message_desc {
+struct MessageDesc {
     data: Vec<u8>,
-    signals: Vec<data_signal_desc>
-    type: packet_type,
+    signals: Vec<DataSignalDesc>,
+    packet_type: PacketType,
     period: u32,
-    interface: RawFd
+    interface: RawFd,
 }
 
 fn main() {
     println!("Hello, world!");
 
-    match init_can_interface("can0") {
-    
-    }
+    match init_can_interface("can0") {}
 }
 
 /* takes the can interface name e.g. can0 and returns a successfully opened
-* interface fd, else the errno returned by the attempt */ 
-fn init_can_interface(interface_name: &str) -> Result<RawFd, i32> {
-    
-}
+* interface fd, else the errno returned by the attempt */
+fn init_can_interface(interface_name: &str) -> Result<RawFd, i32> {}
